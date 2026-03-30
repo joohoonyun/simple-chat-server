be3905fc4d7970d5471b02c8a804134569c58807
juhoon212 <ws5501@naver.com>
2026-03-30 20:35:08 +0900

    feat: mongo-db 연결

--- src/main/kotlin/com/practice/kotlinwebsocket/config/WebSocketConfig.kt
+++ src/main/kotlin/com/practice/kotlinwebsocket/config/WebSocketConfig.kt
@@ -1,4 +1,24 @@
 package com.practice.kotlinwebsocket.config
 
-class WebSocketConfig {
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketConfig: WebSocketMessageBrokerConfigurer {
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/ws-chat")
+            .setAllowedOrigins("*")
+            .withSockJS()
+    }
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.enableSimpleBroker("/topic") // 구독자에게 broadcasting할 때 사용할 prefix
+        //registry.enableStompBrokerRelay("/topic") // 외부 메시지 브로커 사용 시 설정
+        registry.setApplicationDestinationPrefixes("/app") // 서버로 메시지를 보낼 때 사용할 prefix
+    }
 }
--- src/main/kotlin/com/practice/kotlinwebsocket/controller/ChatController.kt
+++ src/main/kotlin/com/practice/kotlinwebsocket/controller/ChatController.kt
@@ -1,4 +1,16 @@
 package com.practice.kotlinwebsocket.controller
 
+import com.practice.kotlinwebsocket.common.ChatMessage
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.stereotype.Controller
+
+@Controller
 class ChatController {
+
+    @MessageMapping("/chat.send") //
+    @SendTo("/topic/public")
+    fun sendMessage(message: ChatMessage): ChatMessage {
+        return message
+    }
 }
